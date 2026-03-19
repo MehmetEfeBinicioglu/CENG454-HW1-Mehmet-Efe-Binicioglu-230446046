15164887cc7b0d8669f3a977e78a5bdc0e4d73ae
Mehmet Efe Binicioglu <mehmet5806efe@gmail.com>
2026-03-19 23:14:22 +0300

    feat: complete Task 3-A through 3-D in FlightController

--- Assets/Scripts/FlightController.cs
+++ Assets/Scripts/FlightController.cs
@@ -1,15 +1,56 @@
+// FlightController.cs
+// CENG 454 - HW1: Sky-High Prototype
+// Author: Mehmet Efe Binicioglu | Student ID: 230446046
+
 using UnityEngine;
 
 public class FlightController : MonoBehaviour
 {
-    [Header("Flight Settings")]
-    public float rotationSpeed = 100f;
+    [SerializeField] private float pitchSpeed = 45f;  // degrees/second
+    [SerializeField] private float yawSpeed   = 45f;  // degrees/second
+    [SerializeField] private float rollSpeed  = 45f;  // degrees/second
+    [SerializeField] private float thrustSpeed = 5f;  // units/second
 
-void Update()
+    // TODO (Task 3-A): Declare a private Rigidbody field named 'rb'.
+    private Rigidbody rb;
+
+    void Start()
     {
-        float pitchInput = Input.GetAxis("Vertical");
-
-        transform.Rotate(Vector3.left * pitchInput * rotationSpeed * Time.deltaTime);
+        // TODO (Task 3-B): Cache GetComponent<Rigidbody>() into 'rb'.
+        rb = GetComponent<Rigidbody>();
+        //Then set rb.freezeRotation = true
+        rb.freezeRotation = true;
     }
 
+    void Update()
+    {
+        HandleRotation();
+        HandleThrust();
+    }
+
+    private void HandleRotation()
+    {
+        // TODO (Task 3-C): Get input for pitch and roll.
+
+        // Pitch: W/S Keys or Up/Down Arrow Keys (Vertical Axis).
+        float pitchInput = Input.GetAxis("Vertical");
+        float pitchAmount = pitchInput * pitchSpeed * Time.deltaTime;
+
+        // Roll: A/D Keys or Left/Right Arrow Keys (Horizontal Axis).
+        float rollInput = Input.GetAxis("Horizontal");
+        float rollAmount = rollInput * rollSpeed * Time.deltaTime;
+
+        transform.Rotate(Vector3.left*pitchAmount); // Pitch
+        transform.Rotate(Vector3.back*rollAmount); // Roll
+    }
+
+    private void HandleThrust()
+    {
+        // TODO (Task 3-D): Get input for thrust.
+        // Thrust: Spacebar.
+        if (Input.GetKey(KeyCode.Space))
+        {
+            transform.Translate(Vector3.forward * thrustSpeed * Time.deltaTime);
+        }
+    }
 }
